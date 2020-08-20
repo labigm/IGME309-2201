4ec1ab71c77b0dcb20815267949efb2cc0620c82
Pierce Habberfield <prh9765@rit.edu>
2020-08-20 14:01:10 -0400

    E01 Made

---
+++ E01/E01/Main.cpp
@@ -0,0 +1,8 @@
+#include "Main.h"
+int main() {
+	std::cout << "Hello World\n";
+	//Ending the program
+	std::cout << "Press enter to finish";
+	getchar();
+	return 0;
+}
---
+++ E01/E01/Main.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef _MAIN_H
+#define _MAIN_H
+#include<iostream>
+#endif//_MAIN_H
